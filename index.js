219f7e9ef92fbca5b8e591e8b056c81ee36a487a
Kevin Gann <kevin.gann@rev.com>
2024-05-06 08:03:46 -0400

    Added graphql server and initial object to query for.

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import express from 'express'
+import { graphqlHTTP } from 'express-graphql';
+import schema from './data/schema';
 
 const PORT = 8080;
 const app = express()
@@ -6,5 +8,13 @@ const app = express()
 app.get('/', (req, res) => {
     res.send('GraphQL is amazing!');
 });
+
+const root = { hello: () => "Hi, I'm kaygee!"}
+
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+}));
 
 app.listen(PORT, () => console.log(`Running server on localhost:${PORT}/graphql`))
